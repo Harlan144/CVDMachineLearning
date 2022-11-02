25d8b440fe9e98ae890710fef1e65313db5c4968
Harlan Stevens <harlanstevens144@gmail.com>
2022-11-02 10:14:18 -0600

    Fixed Dockerfile, added requirements.txt

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM python:3.10-buster
-
+ADD requirements.txt /
 RUN python3 -m pip install --upgrade pip \
- && python3 -m pip install -r requirements.txt
+ && python3 -m pip install -r /requirements.txt
--- test.sh
+++ test.sh
@@ -4,8 +4,6 @@ set -o errexit
 
 docker build -t harlan144/cvd_test .
 
-mkdir -p data
-
-dockerCommand="docker run -i -t --rm --user $(id -u):$(id -g) -v $(pwd):/sandbox -v $(pwd)/data:/data -v /tmp:/tmp --workdir=/sandbox harlan144/cvd_test"
+dockerCommand="docker run -i -t --rm --user $(id -u):$(id -g) -v $(pwd):/sandbox -v /tmp:/tmp --workdir=/sandbox harlan144/cvd_test"
 
 $dockerCommand python3 classifycvd0.py
