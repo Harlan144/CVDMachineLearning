7ab07f8135f8c7332221e37df5ad018293719549
Harlan Stevens <harlanstevens144@gmail.com>
2022-11-03 15:54:53 -0600

    Ran tests

---
+++ saveModel.py
@@ -0,0 +1,83 @@
+import os
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+import matplotlib.pyplot as plt
+
+image_size= (180,180)
+
+def make_model(input_shape, num_classes):
+    inputs = keras.Input(shape=input_shape)
+    # Image augmentation block
+    x = layers.Rescaling(1./255)(inputs)
+
+    # Entry block
+    x = layers.Conv2D(32, 3, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.Conv2D(64, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    previous_block_activation = x  # Set aside residual
+
+    for size in [128, 256, 512, 728]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        # Project residual
+        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    x = layers.SeparableConv2D(1024, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.GlobalAveragePooling2D()(x)
+    x = layers.Dropout(0.5)(x)
+
+    outputs = layers.Dense(1, activation="sigmoid")(x)
+    return keras.Model(inputs, outputs)
+
+# Evaluate the model
+
+
+test_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    "TestImages/",
+    image_size=image_size,
+    shuffle=False
+)
+
+
+
+#Not trained:
+#model = make_model(input_shape=image_size + (3,) , num_classes=2)
+#loss, acc = model.evaluate(test_ds, verbose=2)
+#print("Untrained model, accuracy: {:5.2f}%".format(100 * acc))
+
+
+model = make_model(input_shape=image_size + (3,) , num_classes=2)
+model.compile(
+    optimizer=keras.optimizers.Adam(1e-3),
+    loss="binary_crossentropy",
+    #metrics =["acc"]
+    #switch for AUC
+    metrics=tf.keras.metrics.AUC(),
+)
+
+model.load_weights("Saves/save_at_15.h5")
+
+
+loss, auc = model.evaluate(test_ds, verbose=2)
+print("Untrained model, AUROC: {:5.2f}%".format(100 * auc))
