2ee5645ec31ae051f2bce09fe4d169ccf927a8e1
Harlan Stevens <harlanstevens144@gmail.com>
2023-09-28 11:41:17 -0600

    updated main.sh

--- main.sh
+++ main.sh
@@ -4,6 +4,11 @@ set -o errexit
 
 docker build -t harlan144/cvd_test .
 
+#Run commands interactively
 dockerCommand="docker run -i -t --rm --user $(id -u):$(id -g) -v $(pwd):/sandbox -v /tmp:/tmp --workdir=/sandbox harlan144/cvd_test"
+
+#Uncomment this line to run commands in detached mode
 #dockerCommand="docker run -d -t --rm --user $(id -u):$(id -g) -v $(pwd):/sandbox -v /tmp:/tmp --workdir=/sandbox harlan144/cvd_test"
-$dockerCommand python3 $1 #saveModel_no_transfer.py
+
+#To run any python files, run ./main.sh {name_of_file}. For example, "./main.sh saveModel_no_transfer.py"
+$dockerCommand python3 $1
